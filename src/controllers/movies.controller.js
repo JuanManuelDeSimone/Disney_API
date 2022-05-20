0e6237b65e6aca45366431860e42966d7993da8a
Juan <desimonej@gmail.com>
2022-05-19 23:46:40 -0300

    Commit 20220519_01

--- src/controllers/movies.controller.js
+++ src/controllers/movies.controller.js
@@ -3,10 +3,10 @@ const { Movie } = require('../models/Movie');
 
 const getMovies = async (req,res)=>{
   try {
-    const name = (req.query.name || null);
+    const title = (req.query.title || null);
     let result = null;
-    if(name !== null){
-    const result = await Movie.findAll({
+    if(title !== null){
+      result = await Movie.findAll({
       where:{
         title
       }     
@@ -16,13 +16,71 @@ const getMovies = async (req,res)=>{
           message: "Movie not found",
         });
       }     
+    }else{
+      result = await Movie.findAll();
     }
-
+    await res.json(result);
   } catch (error) {
     console.log(error)
+  }
+};
+const editMovie = async (req,res) =>{
+  try {
+    const title = (req.query.title || null);
+    const {image, date, rate} = req.body;
+    let result = null;
+    if(title !== null){
+      result = await Movie.update({
+        title,
+        image,
+        date,
+        rate
+      },{
+        where: { title }
+      })
+    }
+    if(result.rowCount === 0){
+      return res.status(404).json({
+        message: "Movie not found"
+      })
+    }
+    await res.json(result);
+  } catch (error) {
+    console.log(error)
+  }
+};
+const createMovie = async (req,res) => {
+  try {
+    const {title, image, rate, date} = req.body;
+    const result = await Movie.create({
+      image,
+      title,
+      rate,
+      date
+    })
+    res.json(result);    
+  } catch (error) {
+    console.log(error);
+  }
+};
+const deleteMovie = async (req,res)=>{
+  try {
+    let result = null;
+    const title = (req.query.title || null);
+    if(title !== null){
+      result = await Movie.destroy({
+        where: { title },
+      })
+    }
+    res.json(result);
+  } catch (error) {
+    console.log(error);
   }
 }
 
 module.exports = {
-  getMovies
-}
+  getMovies,
+  editMovie,
+  deleteMovie,
+  createMovie,
+};
--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,8 +1,11 @@
 const { Router } = require("express");
-const { getMovies } = require("../controllers/movies.controller");
+const { getMovies, editMovie, deleteMovie, createMovie } = require("../controllers/movies.controller");
 
 const router = Router();
 
 router.get('/api/movies', getMovies);
+router.put('/api/movies', editMovie);
+router.delete('/api/movies', deleteMovie);
+router.post('/api/movies', createMovie);
 
 module.exports = router;
